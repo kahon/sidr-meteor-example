050be1da2f85fcedda1c70d3b845b1ab6f739a74
Fernando Moreno Ruiz <illoqpaza2@gmail.com>
2015-04-29 17:22:00 +0200

    indent

--- sidr-example.js
+++ sidr-example.js
@@ -1,8 +1,8 @@
 if (Meteor.isClient) {
   $(document).ready(function() {
-    $('#left-menu').sidr({
-      name: 'sidr-left',
-      side: 'left' // By default
-    });
-});
+      $('#left-menu').sidr({
+        name: 'sidr-left',
+        side: 'left' // By default
+      });
+  });
 }
